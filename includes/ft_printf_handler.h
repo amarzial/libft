ddebff8c37f7f2dd79be4936dc18b8b600458159
Alessandro Marzialetti <setti93@gmail.com>
2018-06-19 18:16:22 +0200

    printf: static global table

--- includes/ft_printf_handler.h
+++ includes/ft_printf_handler.h
@@ -6,7 +6,7 @@
 /*   By: amarzial <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/12/08 14:58:20 by amarzial          #+#    #+#             */
-/*   Updated: 2016/12/19 22:08:07 by amarzial         ###   ########.fr       */
+/*   Updated: 2018/06/19 18:15:04 by amarzial         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,25 +22,5 @@ typedef struct	s_handler
 	char	conv;
 	t_func	f;
 }				t_handler;
-
-t_handler		g_table[] =
-{
-	{'%', &ft_printf_char},
-	{'i', &ft_printf_signed_decimal},
-	{'d', &ft_printf_signed_decimal},
-	{'D', &ft_printf_signed_decimal},
-	{'o', &ft_printf_unsigned_octal},
-	{'O', &ft_printf_unsigned_octal},
-	{'u', &ft_printf_unsigned_decimal},
-	{'U', &ft_printf_unsigned_decimal},
-	{'x', &ft_printf_unsigned_hex},
-	{'X', &ft_printf_unsigned_hex},
-	{'p', &ft_printf_unsigned_hex},
-	{'s', &ft_printf_string},
-	{'S', &ft_printf_wstring},
-	{'c', &ft_printf_char},
-	{'C', &ft_printf_wchar},
-	{0, 0}
-};
 
 #endif
--- srcs/ft_printf/srcs/ft_printf_handlings.c
+++ srcs/ft_printf/srcs/ft_printf_handlings.c
@@ -6,7 +6,7 @@
 /*   By: amarzial <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/12/06 17:46:29 by amarzial          #+#    #+#             */
-/*   Updated: 2016/12/13 21:39:10 by amarzial         ###   ########.fr       */
+/*   Updated: 2018/06/19 18:15:24 by amarzial         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,7 +14,25 @@
 #include "ft_printf_internal.h"
 #include "ft_printf_handler.h"
 
-extern t_handler	g_table[];
+static t_handler	g_table[] =
+{
+	{'%', &ft_printf_char},
+	{'i', &ft_printf_signed_decimal},
+	{'d', &ft_printf_signed_decimal},
+	{'D', &ft_printf_signed_decimal},
+	{'o', &ft_printf_unsigned_octal},
+	{'O', &ft_printf_unsigned_octal},
+	{'u', &ft_printf_unsigned_decimal},
+	{'U', &ft_printf_unsigned_decimal},
+	{'x', &ft_printf_unsigned_hex},
+	{'X', &ft_printf_unsigned_hex},
+	{'p', &ft_printf_unsigned_hex},
+	{'s', &ft_printf_string},
+	{'S', &ft_printf_wstring},
+	{'c', &ft_printf_char},
+	{'C', &ft_printf_wchar},
+	{0, 0}
+};
 
 int					ft_printf_handler(t_arg *arg, va_list *lst)
 {
