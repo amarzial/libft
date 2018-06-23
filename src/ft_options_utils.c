a3123502930ca53774e821a9e1deb5e98b8119aa
Alessandro Marzialetti <setti93@gmail.com>
2018-06-23 10:11:07 +0200

    option arguments fix

--- src/ft_options_utils.c
+++ src/ft_options_utils.c
@@ -6,7 +6,7 @@
 /*   By: amarzial <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/06/20 14:45:07 by amarzial          #+#    #+#             */
-/*   Updated: 2018/06/20 18:17:06 by amarzial         ###   ########.fr       */
+/*   Updated: 2018/06/21 12:31:59 by amarzial         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -26,7 +26,7 @@ t_options	opt_find(t_options opts, const char *str)
 	return (opts);
 }
 
-static int	expand_args(t_opt *opt, int size)
+static int	expand_args(t_opt *opt, int size, int *pos)
 {
 	int		current;
 	char	**args;
@@ -38,6 +38,8 @@ static int	expand_args(t_opt *opt, int size)
 	if ((args = (char**)realloc(opt->args, \
 					sizeof(char*) * (current + size + 1))) == NULL)
 		return (0);
+    opt->args = args;
+    *pos = current;
 	return (1);
 }
 
@@ -46,22 +48,23 @@ int			insert_args(t_options t, int *i, int ac, const char **av)
 	t_opt	*opt;
 	int		count;
 	int		size;
+    int     pos;
 
 	opt = (t_opt*)t->content;
-	opt->used += 1;
+	opt->used = 1;
 	size = opt->nargs >= 0 ? opt->nargs : ac - 1 - *i;
 	if (size == 0)
 		return (OPT_OK);
 	if (size > ac - 1 - *i)
 		return (OPT_BAD_NARGS);
-	if (!expand_args(opt, size))
+	if (!expand_args(opt, size, &pos))
 		return (OPT_BAD_ALLOC);
-	opt->args[size] = NULL;
+	opt->args[pos + size] = NULL;
 	count = 0;
 	while (count < size)
 	{
 		*i += 1;
-		if ((opt->args[count++] = ft_strdup(av[*i])) == NULL)
+		if ((opt->args[pos + count++] = ft_strdup(av[*i])) == NULL)
 			return (OPT_BAD_ALLOC);
 	}
 	return (OPT_OK);
